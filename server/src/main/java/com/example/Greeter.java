92f335cf1bac3271c4b74c8b4f62ceca05aad0b6
nadiya kochura <nkochura@us.ibm.com>
2018-03-03 11:30:46 -0500

    fix warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,10 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * This is greet method.
+   */
+  final public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
